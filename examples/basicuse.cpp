70dab36451902b457de5b5b2fb3db6d17742c314
pasyg <pgekle@gmail.com>
2023-12-29 00:05:10 +0100

    examples

---
+++ examples/basicuse.cpp
@@ -0,0 +1,50 @@
+#include "wrapsire.hpp"
+
+#include "agents/random.hpp"
+
+pkmn_choice choose(
+   pkmn_gen1_battle *battle,
+   pkmn_psrng *random,
+   pkmn_player player,
+   pkmn_choice_kind request,
+   pkmn_choice choices[])
+{
+   uint8_t n = pkmn_gen1_battle_choices(battle, player, request, choices, PKMN_CHOICES_SIZE);
+   // Technically due to Generation I's Transform + Mirror Move/Metronome PP
+   // error if the battle contains Pokémon with a combination of Transform,
+   // Mirror Move/Metronome, and Disable its possible that there are no
+   // available choices (softlock), though this is impossible here given that
+   // our example battle involves none of these moves
+   assert(n > 0);
+   // pkmn_gen1_battle_choices determines what the possible choices are - the
+   // simplest way to choose an option here is to just use the PSRNG to pick one
+   // at random
+   return choices[(uint64_t)pkmn_psrng_next(random) * n / 0x100000000];
+}
+
+int main(int argc, char **argv)
+{
+   using namespace engine::RBY;
+   
+   auto battle = engine::RBY::miedon_zapjynx;
+   pkmn_result result = PKMN_RESULT_NONE;
+   pkmn_choice c1;
+   pkmn_choice c2;
+   pkmn_psrng random;
+   uint64_t seed = 0;
+   pkmn_psrng_init(&random, seed);
+   pkmn_choice choices[PKMN_CHOICES_SIZE];
+
+   battle.init();
+
+   while(!pkmn_result_type(battle.result))
+   {
+      c1 = choose(battle(), &random, PKMN_PLAYER_P1, pkmn_result_p1(battle.result), choices);
+      c2 = choose(battle(), &random, PKMN_PLAYER_P2, pkmn_result_p2(battle.result), choices);
+      battle.play_turn(c1, c2);
+   }
+
+   battle.print_result();
+
+   return 0;
+}
---
+++ examples/randomagent.cpp
@@ -0,0 +1,75 @@
+#include "wrapsire.hpp"
+
+#include "agents/random.hpp"
+
+
+// Can be used to randomly pick a team out of a container
+template<typename Container>
+typename Container::value_type random_team(const Container& teams)
+{
+   static std::random_device rd;
+   static std::mt19937 gen(rd());
+   static std::uniform_int_distribution<> distrib(0, teams.size() - 1);
+   return teams[distrib(gen)];
+}
+
+int main()
+{
+    using namespace engine::RBY;
+
+    int battles = 10000;
+    uint64_t seed = 0;
+
+    // The probabilities for the agents to select a move over a switch
+    double agent1_prob = 0.5;
+    double agent2_prob = 0.75;
+
+    auto Agent1 = RandomAgentRBY(agent1_prob);
+    auto Agent2 = RandomAgentRBY(agent2_prob);
+
+    // Sample teams
+    std::vector<rby_side> teams = {miedon, zamdon, zapjynx, miebro, zamjolt, zambro, miejolt};
+
+    // Counting variables for battle results
+    int A=0, B=0, C=0;
+
+    // RNG for battle seeds
+    std::random_device rd;
+    std::mt19937_64 gen(rd());
+    std::uniform_int_distribution<uint64_t> distrib;
+
+    // play out each matchup #battles of time
+    // and count the wins/ties
+    auto battle = rby_battle(&Agent1, &Agent2, zamdon, miebro, distrib(gen));
+    for(auto&& team : teams)
+    {
+        for(auto&& team_ : teams)
+        {
+            for(int i = 0; i < battles; ++i)
+            {
+                battle = rby_battle(&Agent1, &Agent2, team, team_, distrib(gen));
+                battle.play_battle();
+
+                if(battle.result == PKMN_RESULT_WIN) {
+                    ++A;
+                } else if(battle.result == PKMN_RESULT_LOSE) {
+                    ++B;
+                } else if(battle.result == PKMN_RESULT_TIE) {
+                    ++C;
+                } else {
+                    std::cout << "ERROR" << std::endl;
+                    assert(false);
+                }
+            }
+        }
+    }
+
+    int totalBattles = teams.size() * teams.size() * battles;
+    
+    std::cout << "After  " << totalBattles << " games:\n";
+    std::cout << agent1_prob * 100 << "% move probability\twins: " << A << "\tPercentage: " << ((double)A / totalBattles) * 100 << "%\n";
+    std::cout << agent2_prob * 100 << "% move probability\twins: " << B << "\tPercentage: " << ((double)B / totalBattles) * 100 << "%\n";
+    std::cout << "\t\t\tTies:  "   << C << "\tPercentage: " << ((double)C / totalBattles) * 100 << "%\n";
+
+    return 0;
+}
